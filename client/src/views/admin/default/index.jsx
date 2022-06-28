fdb4945d7c9ac08ce3baf78d4a9bf4e8e6e5f948
stallingerl <lena.stallinger@webanizer.de>
2022-06-28 11:08:02 +0200

    added total bookings and total electricity

--- client/src/views/admin/default/index.jsx
+++ client/src/views/admin/default/index.jsx
@@ -43,6 +43,8 @@ import {
   MdAttachMoney,
   MdBarChart,
   MdFileCopy,
+  MdFilePresent,
+  MdPower
 } from "react-icons/md";
 import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
@@ -56,7 +58,7 @@ import {
   columnsDataComplex,
 } from "views/admin/default/variables/columnsData";
 import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-//import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
+
 
 export default function UserReports() {
 
@@ -80,93 +82,109 @@ export default function UserReports() {
     })();
   }, []);
 
+  function calculateTotalEnergy(data) {
+    let energyData = []
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].energy.length; j++) {
+        energyData.push(data[i].energy[j])
+      }
+    }
+    let totalElectricityAmount = 0 
+    for (let i = 0; i < energyData.length; i++) {
+      totalElectricityAmount += energyData[i].energy_kwh
+    }
+    return `${totalElectricityAmount} kwH`
+  }
+
+
   // Chakra Color Mode
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
-        gap='20px'
-        mb='20px'>
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
-              }
-            />
-          }
-          name='Earnings'
-          value='$350.4'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
-              }
-            />
-          }
-          name='Spend this month'
-          value='$642.39'
-        />
-        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-        <MiniStatistics
-          endContent={
-            <Flex me='-16px' mt='10px'>
-              <FormLabel htmlFor='balance'>
-                <Avatar src={Usa} />
-              </FormLabel>
-              <Select
-                id='balance'
-                variant='mini'
-                mt='5px'
-                me='0px'
-                defaultValue='usd'>
-                <option value='usd'>USD</option>
-                <option value='eur'>EUR</option>
-                <option value='gba'>GBA</option>
-              </Select>
-            </Flex>
-          }
-          name='Your balance'
-          value='$1,000'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-            />
-          }
-          name='New Tasks'
-          value='154'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
-              }
-            />
-          }
-          name='Total Projects'
-          value='2935'
-        />
-      </SimpleGrid>
-
+      {loading == false && (
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
+          gap='20px'
+          mb='20px'>
+          <MiniStatistics
+            startContent={
+              <IconBox
+                w='56px'
+                h='56px'
+                bg={boxBg}
+                icon={
+                  <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
+                }
+              />
+            }
+            name='Earnings'
+            value='$350.4'
+          />
+          <MiniStatistics
+            startContent={
+              <IconBox
+                w='56px'
+                h='56px'
+                bg={boxBg}
+                icon={
+                  <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
+                }
+              />
+            }
+            name='Spend this month'
+            value='$642.39'
+          />
+          <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
+          <MiniStatistics
+            endContent={
+              <Flex me='-16px' mt='10px'>
+                <FormLabel htmlFor='balance'>
+                  <Avatar src={Usa} />
+                </FormLabel>
+                <Select
+                  id='balance'
+                  variant='mini'
+                  mt='5px'
+                  me='0px'
+                  defaultValue='usd'>
+                  <option value='usd'>USD</option>
+                  <option value='eur'>EUR</option>
+                  <option value='gba'>GBA</option>
+                </Select>
+              </Flex>
+            }
+            name='Your balance'
+            value='$1,000'
+          />
+          <MiniStatistics
+            startContent={
+              <IconBox
+                w='56px'
+                h='56px'
+                bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
+                icon={<Icon w='28px' h='28px' as={MdPower} color='white' />}
+              />
+            }
+            name='Total Booked Electricity'
+            value={calculateTotalEnergy(tableDataComplex)}
+          />
+          <MiniStatistics
+            startContent={
+              <IconBox
+                w='56px'
+                h='56px'
+                bg={boxBg}
+                icon={
+                  <Icon w='32px' h='32px' as={MdFilePresent} color={brandColor} />
+                }
+              />
+            }
+            name='Total Electricity Bookings'
+            value={tableDataComplex.length}
+          />
+        </SimpleGrid>
+      )}
       <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px' >
         <TotalSpent />
         <WeeklyRevenue />
@@ -179,11 +197,11 @@ export default function UserReports() {
         </SimpleGrid>
       </SimpleGrid>
       <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px' >
-      {loading == false && (
-        <ComplexTable 
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
-        />)}
+        {loading == false && (
+          <ComplexTable
+            columnsData={columnsDataComplex}
+            tableData={tableDataComplex}
+          />)}
         <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
           <Tasks />
           <MiniCalendar h='100%' minW='100%' selectRange={false} />
